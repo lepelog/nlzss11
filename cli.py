ebbd7a2c647b49cec5d97ec6793a57360e89e2fe
lepelog <lepelog4@gmail.com>
2020-05-22 21:38:00 +0200

    fix cli and maintainer info

--- cli.py
+++ cli.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 import argparse
 from pathlib import Path
-import syaz0
+import nlzss11
 import sys
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser("syaz0", description="Compress or decompress Yaz0 data")
+    parser = argparse.ArgumentParser("nlzss11", description="Compress or decompress Nintendo LZSS11 data")
     parser.add_argument("-d", "--decompress", help="Decompress", action="store_true", default=False)
     parser.add_argument("-c", "--stdout", help="Write output to stdout", action="store_true", default=False)
     parser.add_argument("-l", "--level", help="Compression level (6-9)",
                         type=int, choices=[6, 7, 8, 9], default=9)
-    parser.add_argument("-a", "--alignment",
-                        help="Data alignment hint (for compression)", type=int, default=0)
     parser.add_argument("file")
 
     args = parser.parse_args()
@@ -20,14 +18,14 @@ def main() -> None:
     with open(args.file, "rb") as f:
         data = f.read()
 
-    if args.decompress or args.file.endswith(".yaz0"):
-        result = syaz0.decompress(data)
-        name = args.file.replace(".yaz0", "")
+    if args.decompress or args.file.endswith(".LZ"):
+        result = nlzss11.decompress(data)
+        name = args.file.replace(".LZ", "")
         if Path(name).exists():
             name += ".decomp"
     else:
-        result = syaz0.compress(data, data_alignment=args.alignment, level=args.level)
-        name = args.file + ".yaz0"
+        result = nlzss11.compress(data, level=args.level)
+        name = args.file + ".LZ"
 
     if args.stdout:
         sys.stdout.buffer.write(result)
