36e6e0ebd3a850570efdebfd783c0b3e202b4d5b
lepelog <25211966+lepelog@users.noreply.github.com>
2022-02-07 18:48:55 +0100

    version bump and try submodule updates

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ cmdclass["build_ext"] = CMakeBuild
 
 setuptools.setup(
     name="nlzss11",
-    version="1.3",
+    version="1.4",
     cmdclass=cmdclass,
     author="lepelog",
     description="Library for data (de)compression using Nintendo's lzss11 algorithm",
